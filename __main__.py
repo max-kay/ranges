a1c535f16dc80527d8f46ae3069cd37734a13131
Max Krummenacher <maxkrummenacher@bluewin.ch>
2024-12-23 12:15:37 +0100

    Initial Commit

---
+++ __main__.py
@@ -0,0 +1,203 @@
+import drawsvg as draw
+from music import Instrument
+from elements import G_CLEF, NOTE_HEAD, CLEF_WIDTH
+
+TEXT_STYLING = {
+    "font": "Cochin",
+}
+
+CLEF_OFFSET = 10
+MARGIN = 100
+STAFF_LINE_SPACING = 17
+STAFF_STROKE_WIDTH = 1.6
+SECONDARY_STROKE_WIDTH = 1.3
+FORMAT = (3508, 2480)  # A4
+MIN_X = MARGIN
+MAX_X = FORMAT[0] - MARGIN
+
+
+def generate_staff(instruments: list[Instrument]) -> draw.Drawing:
+    lowest_full = -10
+    highest_full = 10
+    img = draw.Drawing(*FORMAT)
+    img.append(draw.Rectangle(0, 0, *FORMAT, fill="white"))
+    min_spos = min(
+        map(lambda inst: inst.min_sounding_pitch(), instruments)
+    ).to_staff_position()
+    min_spos = min(-10, min_spos)
+    max_spos = max(
+        map(lambda inst: inst.max_sounding_pitch(), instruments)
+    ).to_staff_position()
+    max_spos = max(10, max_spos)
+
+    display_lower_f = False
+    if min_spos < -10 - 14 + 4:
+        display_lower_f = True
+        lowest_full = -14 - 10
+        min_spos = min(min_spos, -10 - 14)
+
+    display_upper_g = False
+    if min_spos < 10 + 14 - 4:
+        display_upper_g = True
+        highest_full = 14 + 10
+        max_spos = max(max_spos, 10 + 14)
+
+    center_spos = (min_spos + max_spos) / 2
+
+    middle_c_line = FORMAT[1] / 2 + STAFF_LINE_SPACING / 2 * center_spos
+
+    staff_lines = draw.Group()
+
+    for i in range(min_spos, max_spos + 1):
+        if i % 2 == 1:
+            continue
+        height = middle_c_line - STAFF_LINE_SPACING / 2 * i
+        dashes = "none"
+        if i > 10 or i < -10 or i == 0:
+            dashes = "10 30"
+
+        if display_upper_g and 2 + 14 <= i <= 10 + 14:
+            dashes = "none"
+        if display_lower_f and -2 - 14 >= i >= -10 - 14:
+            dashes = "none"
+
+        staff_lines.append(
+            draw.Line(
+                MIN_X,
+                height,
+                MAX_X,
+                height,
+                stroke="black",
+                stroke_width=STAFF_STROKE_WIDTH,
+                stroke_dasharray=dashes,
+            )
+        )
+
+    img.append(staff_lines)
+    img.append(
+        draw.Group(
+            children=[G_CLEF],
+            transform=f"translate({CLEF_OFFSET + MARGIN} {middle_c_line-2*STAFF_LINE_SPACING}) scale({STAFF_LINE_SPACING/2})",
+        )
+    )
+
+    x_offset = CLEF_OFFSET + CLEF_WIDTH * STAFF_LINE_SPACING / 2
+    instr_width = (FORMAT[0] - 2 * MARGIN - x_offset) / len(instruments)
+    for i, instr in enumerate(instruments):
+        x = MARGIN + x_offset + i * instr_width
+        if i != len(instruments) - 1:
+            img.append(
+                draw.Line(
+                    x + instr_width,
+                    middle_c_line - STAFF_LINE_SPACING / 2 * highest_full,
+                    x + instr_width,
+                    middle_c_line - STAFF_LINE_SPACING / 2 * lowest_full,
+                    stroke_width=SECONDARY_STROKE_WIDTH,
+                    stroke="black",
+                )
+            )
+        else:
+            img.append(
+                draw.Line(
+                    x + instr_width - 4 * SECONDARY_STROKE_WIDTH,
+                    middle_c_line - STAFF_LINE_SPACING / 2 * highest_full,
+                    x + instr_width - 4 * SECONDARY_STROKE_WIDTH,
+                    middle_c_line - STAFF_LINE_SPACING / 2 * lowest_full,
+                    stroke_width=SECONDARY_STROKE_WIDTH,
+                    stroke="black",
+                )
+            )
+            img.append(
+                draw.Line(
+                    x + instr_width,
+                    middle_c_line
+                    - STAFF_LINE_SPACING / 2 * highest_full
+                    - STAFF_STROKE_WIDTH / 2,
+                    x + instr_width,
+                    middle_c_line
+                    - STAFF_LINE_SPACING / 2 * lowest_full
+                    + STAFF_STROKE_WIDTH / 2,
+                    stroke_width=SECONDARY_STROKE_WIDTH * 2,
+                    stroke="black",
+                )
+            )
+        instrument = draw.Group(
+            [instr_graph(instr, instr_width / (STAFF_LINE_SPACING / 2), -min_spos)],
+            transform=f"translate({x} {middle_c_line}) scale({STAFF_LINE_SPACING/2})",
+        )
+        img.append(instrument)
+    return img
+
+
+INST_MARGIN = 5
+MAIN_FONT_SIZE = 3
+OVERLAP_OFFSET = 0.66
+
+def instr_graph(instr: Instrument, width: float, max_y: float) -> draw.Group:
+    """
+    This function returns a svg group with all information between x=0 and x=width including margins
+    Pitch C4 (middle C) is at y=0 and D4 at y=-1
+    """
+    group = draw.Group()
+    note_position = width - INST_MARGIN
+    ranges = instr.get_sounding_pitch_ranges()
+    current_offset = 0
+    for i in range(len(ranges)):
+        group.append(
+            draw.Group(
+                [NOTE_HEAD],
+                transform=f"translate({note_position-current_offset*OVERLAP_OFFSET} {-ranges[i].start.to_staff_position()})",
+            )
+        )
+        if i != len(ranges)-1 and ranges[i].end == ranges[i + 1].start:
+            current_offset = 0
+            continue
+        elif i != len(ranges)-1 and ranges[i].end.to_staff_position()-ranges[i + 1].start.to_staff_position() < 3 :
+            current_offset = 1
+        else:
+            current_offset = 0
+        group.append(
+            draw.Group(
+                [NOTE_HEAD],
+                transform=f"translate({note_position+current_offset*OVERLAP_OFFSET} {-ranges[i].end.to_staff_position()})",
+            )
+        )
+    group.append(
+        draw.Group(
+            children=[
+                draw.Text(
+                    instr.name,
+                    x=0,
+                    y=0,
+                    font_size=MAIN_FONT_SIZE,
+                    text_anchor="end",
+                    **TEXT_STYLING,
+                )
+            ],
+            transform=f"translate({note_position} {max_y+3}) rotate(-45)",
+        )
+    )
+    return group
+
+
+def from_names(names: list[str]) -> draw.Drawing:
+    return generate_staff([Instrument.from_file(f"insts/{name}.txt") for name in names])
+
+
+def parsing_test():
+    _trombone = Instrument.from_file("tb.txt")
+
+
+def grafics_test():
+    img = from_names(
+        ["lisanne", "fl", "cl", "as", "ts", "bs", "tp", "tb", "btb", "git", "b"]
+    )
+    img.save_svg("out.svg")
+
+
+def main():
+    grafics_test()
+
+
+if __name__ == "__main__":
+    main()
---
+++ music.py
@@ -0,0 +1,370 @@
+from functools import total_ordering
+
+NOTE_NAMES = [chr(ord("A") + i) for i in range(7)]
+NOTE_TO_STAFF = {
+    "C": 0,
+    "D": 1,
+    "E": 2,
+    "F": 3,
+    "G": 4,
+    "A": 5,
+    "B": 6,
+}
+STAFF_TO_NOTE = {
+    0: "C",
+    1: "D",
+    2: "E",
+    3: "F",
+    4: "G",
+    5: "A",
+    6: "B",
+}
+
+NOTE_TO_MIDI_PITCH = {
+    "C": 0,
+    "D": 2,
+    "E": 4,
+    "F": 5,
+    "G": 7,
+    "A": 9,
+    "B": 11,
+}
+MIDI_PITCH_TO_NOTE = {
+    0: "C",
+    1: "C#",
+    2: "D",
+    3: "Eb",
+    4: "E",
+    5: "F",
+    6: "F#",
+    7: "G",
+    8: "Ab",
+    9: "A",
+    10: "Bb",
+    11: "B",
+}
+ACCIDENTALS = ["&", "b", "", "#", "+"]
+ACCIDENTAL_MAP = {
+    "b": -1,
+    "#": 1,
+    "": 0,
+    "+": 2,
+    "&": -2,
+}
+
+INT_MODIFIER = ["j", "m", "a", "d", ""]
+
+
+class Interval:
+    def __init__(
+        self, number: int, down: bool = False, modifier: str | None = None
+    ) -> None:
+        modifier = modifier if modifier else ""
+        assert number >= 1
+        normalized_num = (number - 1) % 7 + 1
+        if normalized_num in [1, 4, 5]:
+            assert modifier != "j" and modifier != "m"
+        else:
+            assert modifier != ""
+        assert modifier in INT_MODIFIER
+        self.number = number
+        self.down = down
+        self.modifier = modifier
+
+    @classmethod
+    def from_str(cls, string: str):
+        """
+        Parse an interval from its string representation.
+        Format: [Direction (optional '-')][Modifier][Number]
+        Examples: 'm3', '-a4', '5'
+        """
+        if string.startswith("-"):
+            down = True
+            string = string[1:]
+        else:
+            down = False
+
+        if string[0] in INT_MODIFIER:
+            modifier = string[0]
+            string = string[1:]
+        else:
+            modifier = None
+
+        number = int(string)
+
+        return cls(number, down, modifier)
+
+    def __repr__(self):
+        direction = "-" if self.down else ""
+        return f"{self.modifier}{self.number}{direction}"
+
+    def get_dir(self) -> int:
+        return -1 if self.down else 1
+
+    def to_halftones(self) -> int:
+        octave = (self.number - 1) // 7
+        number = (self.number - 1) % 7 + 1
+        shift = 0
+        if number in [1, 4, 5]:
+            match number:
+                case 1:
+                    pass
+                case 4:
+                    shift += 5
+                case 5:
+                    shift += 7
+                case _:
+                    assert False, "unreachable"
+            match self.modifier:
+                case "":
+                    pass
+                case "a":
+                    shift += 1
+                case "d":
+                    shift += -1
+        else:
+            # doing shift for minor interval
+            match number:
+                case 2:
+                    shift += 1
+                case 3:
+                    shift += 3
+                case 6:
+                    shift += 8
+                case 7:
+                    shift += 10
+            match self.modifier:
+                case "d":
+                    shift += -1
+                case "m":
+                    pass
+                case "j":
+                    shift += 1
+                case "a":
+                    shift += 2
+                case _:
+                    assert False, "unreachable"
+        direction = -1 if self.down else 1
+        return direction * (shift + octave * 12)
+
+
+@total_ordering
+class Pitch:
+    def __init__(self, note: str, octave: int, accidental: str | None = None) -> None:
+        self.note = note
+        self.octave = octave
+        self.accidental = accidental if accidental else ""
+
+    @classmethod
+    def from_str(cls, string: str):
+        note = string[0]
+        assert ord("A") <= ord(string[0]) <= ord("G"), f"invalid note name {note}"
+        if not string[1].isdigit():
+            accidental = string[1]
+            assert accidental in ACCIDENTALS, f"invalid accidental {accidental}"
+            octave = int(string[2:])
+            return cls(note, octave, accidental)
+        else:
+            octave = int(string[1:])
+            return cls(note, octave)
+
+    def __str__(self) -> str:
+        return f"{self.note}{self.accidental}{self.octave}"
+
+    def to_midi_pitch(self) -> int:
+        return (
+            NOTE_TO_MIDI_PITCH[self.note]
+            + ACCIDENTAL_MAP[self.accidental]
+            + self.octave * 12
+            + 12
+        )
+
+    @classmethod
+    def from_midi_pitch(cls, pitch: int):
+        octave = pitch // 12 - 1
+        note = MIDI_PITCH_TO_NOTE[pitch % 12]
+        return cls.from_str(f"{note}{octave}")
+
+    def to_staff_position(self) -> int:
+        """
+        The staff position puts C4 at 0 and D4 at 1
+        """
+        octave_dif = self.octave - 4  # C4 is middle C
+        return octave_dif * 7 + NOTE_TO_STAFF[self.note]
+
+    @classmethod
+    def from_staff_position(cls, pos: int, accidental: str | None = None):
+        octave = pos // 7 + 4
+        note = STAFF_TO_NOTE[pos % 7]
+        return cls(note, octave, accidental)
+
+    def get_accidental(self) -> str:
+        return self.accidental
+
+    def transposed(self, interval: Interval):
+        shift = interval.get_dir() * (interval.number - 1)
+        position = self.to_staff_position() + shift
+        current_midi = Pitch.from_staff_position(position).to_midi_pitch()
+        correct_midi = self.to_midi_pitch() + interval.to_halftones()
+        additional_shift = correct_midi - current_midi
+        try:
+            accidental = ACCIDENTALS[additional_shift + 2]
+            return Pitch.from_staff_position(position, accidental)
+        except (
+            IndexError
+        ):  # happens if the needed accidental exceeds double sharps and double flats
+            return Pitch.from_midi_pitch(correct_midi)
+
+    def __eq__(self, other) -> bool:
+        return self.to_midi_pitch() == other.to_midi_pitch()
+
+    def __lt__(self, other) -> bool:
+        return self.to_midi_pitch() < other.to_midi_pitch()
+
+
+class PartialRange:
+    def __init__(self, start: Pitch, end: Pitch, descr: str) -> None:
+        assert start <= end, "start must be smaller than end"
+        self.start = start
+        self.end = end
+        self.descr = descr
+
+    def __lt__(self, other) -> bool:
+        assert self.start != other.start
+        return self.start < other.start
+
+    def __str__(self) -> str:
+        return f"{self.start} {self.end} {self.descr}"
+
+    def transposed(self, interval: Interval):
+        return PartialRange(
+            self.start.transposed(interval), self.end.transposed(interval), self.descr
+        )
+
+    @classmethod
+    def from_str(cls, string: str):
+        start, end, *rest = string.split()
+        start = Pitch.from_str(start)
+        end = Pitch.from_str(end)
+        return cls(start, end, " ".join(rest))
+
+
+class Instrument:
+    def __init__(
+        self,
+        name: str,
+        ranges: list[PartialRange],
+        transposition: Interval | None = None,
+    ) -> None:
+        self.name = name
+        self.ranges = sorted(ranges)
+        self.transposition = transposition if transposition else Interval.from_str("1")
+        for i in range(0, len(self.ranges) - 1):
+            assert (
+                self.ranges[i].end <= self.ranges[i + 1].start
+            ), f"{self.name} has invalid ranges"
+
+    def __str__(self):
+        ranges_str = "\n".join(str(r) for r in self.ranges)
+        transposition = (
+            f"{self.transposition}\n" if self.transposition.to_halftones() != 0 else ""
+        )
+        return f"{self.name}\n{transposition}{ranges_str}"
+
+    def min_sounding_pitch(self) -> Pitch:
+        return self.ranges[0].start.transposed(self.transposition)
+
+    def max_sounding_pitch(self) -> Pitch:
+        return self.ranges[-1].end.transposed(self.transposition)
+
+    def get_sounding_pitch_ranges(self) -> list[PartialRange]:
+        out = []
+        for r in self.ranges:
+            out.append(r.transposed(self.transposition))
+        return out
+
+    @classmethod
+    def from_file(cls, path: str):
+        with open(path, encoding="utf8", mode="r") as file:
+            lines = [line.strip() for line in file.readlines() if line.strip() != ""]
+        name = lines[0]
+        if lines[1].startswith("tran"):
+            try:
+                transposition = Interval.from_str(lines[1].split()[1])
+            except:
+                print(f"failed to parse transposition of {name} file: {path}")
+                assert False
+            lines = lines[2:]
+        else:
+            transposition = Interval.from_str("1")
+            lines = lines[1:]
+        ranges = []
+        for line in lines:
+            try:
+                ranges.append(PartialRange.from_str(line))
+            except:
+                print(f"failed to parse range of {name} file: {path}\n {line}")
+                assert False
+        return cls(name, ranges, transposition)
+
+
+def pitch_test():
+    assert Pitch.from_str("C0").to_midi_pitch() == 12
+    assert Pitch.from_str("E0").to_midi_pitch() == 12 + 4
+    assert Pitch.from_str("C1").to_midi_pitch() == 24
+    assert Pitch.from_str("Bb0").to_midi_pitch() == 24 - 2
+    assert Pitch.from_str("C#1").to_midi_pitch() == 24 + 1
+    assert str(Pitch.from_str("C#1")) == "C#1"
+    assert Pitch.from_str("C4").to_staff_position() == 0
+    assert Pitch.from_str("D4").to_staff_position() == 1
+    assert Pitch.from_str("D#4").to_staff_position() == 1
+    assert Pitch.from_str("Db4").to_staff_position() == 1
+    assert Pitch.from_str("Bb3").to_staff_position() == -1
+    assert Pitch.from_str("C5").to_staff_position() == 7
+    assert Pitch.from_str("C3").to_staff_position() == -7
+
+    note = Pitch.from_str("D4")
+    assert note == Pitch.from_midi_pitch(note.to_midi_pitch())
+    assert str(note) == str(Pitch.from_midi_pitch(note.to_midi_pitch()))
+    note = Pitch.from_str("C3")
+    assert note == Pitch.from_midi_pitch(note.to_midi_pitch())
+    assert str(note) == str(Pitch.from_midi_pitch(note.to_midi_pitch()))
+    note = Pitch.from_str("Bb2")
+    assert note == Pitch.from_midi_pitch(note.to_midi_pitch())
+    assert str(note) == str(Pitch.from_midi_pitch(note.to_midi_pitch()))
+    note = Pitch.from_str("F#4")
+    assert note == Pitch.from_midi_pitch(note.to_midi_pitch())
+    assert str(note) == str(Pitch.from_midi_pitch(note.to_midi_pitch()))
+
+
+def interval_test():
+    assert Interval.from_str("1").to_halftones() == 0
+    assert Interval.from_str("m2").to_halftones() == 1
+    assert Interval.from_str("j2").to_halftones() == 2
+    assert Interval.from_str("m3").to_halftones() == 3
+    assert Interval.from_str("j3").to_halftones() == 4
+    assert Interval.from_str("4").to_halftones() == 5
+    assert Interval.from_str("a4").to_halftones() == 6
+    assert Interval.from_str("d5").to_halftones() == 6
+    assert Interval.from_str("5").to_halftones() == 7
+    assert Interval.from_str("m6").to_halftones() == 8
+    assert Interval.from_str("j6").to_halftones() == 9
+    assert Interval.from_str("m7").to_halftones() == 10
+    assert Interval.from_str("j7").to_halftones() == 11
+
+    assert Interval.from_str("a3").to_halftones() == 5
+    assert Interval.from_str("d3").to_halftones() == 2
+
+    assert Interval.from_str("-m6").to_halftones() == -8
+    assert Interval.from_str("m13").to_halftones() == 20
+
+    assert str(Pitch.from_str("C0").transposed(Interval.from_str("m3"))) == "Eb0"
+    assert str(Pitch.from_str("C0").transposed(Interval.from_str("j3"))) == "E0"
+    assert str(Pitch.from_str("Bb1").transposed(Interval.from_str("j3"))) == "D2"
+    assert str(Pitch.from_str("B1").transposed(Interval.from_str("j3"))) == "D#2"
+    assert str(Pitch.from_str("C1").transposed(Interval.from_str("5"))) == "G1"
+    assert str(Pitch.from_str("C1").transposed(Interval.from_str("8"))) == "C2"
+
+
+pitch_test()
+interval_test()
